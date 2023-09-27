1d487427ee42fde78af8ffba2867e907c20453c4
Maarten Brakkee <contact@maartenbrakkee.nl>
2023-09-27 12:17:08 +0200

    ignore prettier

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,14 @@
-export const wrappedTemplate = (
-  template: string | undefined,
-  slotName: string
-) => {
-  const templateWithDefault = template ?? `{{ args.${slotName} }}`;
 
-  const wrap = (contents: string) =>
-    `<template #${slotName}>\n${contents}\n</template>`;
+export const wrappedTemplate = (template: string | undefined, slotName: string) => {
 
-  const wrappedTemplate = templateWithDefault.includes(`<template #${slotName}`)
-    ? templateWithDefault
-    : wrap(templateWithDefault);
+  const templateWithDefault = template ?? `{{ args.${slotName} }}`
 
-  return wrappedTemplate.replace(
-    /<template #default>(\n*)(.*)(\n*)<\/template>/gimsu,
-    "$2"
-  );
-};
+  const wrap = (contents: string) => `<template #${slotName}>\n${contents}\n</template>`
 
-export const SLOTS_CATEGORY_NAME = "slots";
+  const wrappedTemplate = templateWithDefault.includes(`<template #${slotName}`) ?
+  templateWithDefault : wrap(templateWithDefault)
+
+  return wrappedTemplate.replace(/<template #default>(\n*)(.*)(\n*)<\/template>/gimsu, '$2')
+}
+
+export const SLOTS_CATEGORY_NAME = 'slots'
